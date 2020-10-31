f5ae3a50c9ad70808f6b9b59d65726a27ec6064c
Luis Cubillo <luis.cubillo.soto@gmail.com>
2020-10-30 22:06:33 -0600

    Handle routes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,42 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :update, :destroy]
+  # GET /questions
+  def index
+    @questions = Question.all
+    json_response(@questions)
+  end
+
+  # POST /questions
+  def create
+    @question = Question.create!(questions_params)
+    json_response(@question, :created)
+  end
+
+  # GET /questions/:id
+  def show
+    json_response(@question)
+  end
+
+  # PUT /questions/:id
+  def update
+    @question.update(questions_params)
+    head :no_content
+  end
+
+  # DELETE /questions/:id
+  def destroy
+    @todo.destroy
+    head :no_content
+  end
+
+  private
+
+  def questions_params
+    # whitelist params
+    params.permit(:user_id, :title, :body)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
 end
