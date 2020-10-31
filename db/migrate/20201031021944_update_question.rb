ff08f68e86b037ece1b0b571a55c10c34407aee3
Luis Cubillo <luis.cubillo.soto@gmail.com>
2020-10-30 22:30:17 -0600

    Fix on question migrate

--- db/migrate/20201031021944_update_question.rb
+++ db/migrate/20201031021944_update_question.rb
@@ -3,6 +3,5 @@ class UpdateQuestion < ActiveRecord::Migration[6.0]
     add_column :questions, :user_id, :integer
     add_column :questions, :title, :string
     add_column :questions, :body, :string
-    add_foreign_key :questions, :users
   end
 end
